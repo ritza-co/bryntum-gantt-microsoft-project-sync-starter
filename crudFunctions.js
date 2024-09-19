597c3fee255a87cfed81996e4be4ed8880464ddf
Matthew Clarke <m_d_clarke@hotmail.com>
2024-09-19 17:28:31 +0200

    add: completed gantt

---
+++ crudFunctions.js
@@ -0,0 +1,388 @@
+import { ensureScope, getToken } from './auth';
+
+export async function getProjectTasks() {
+    try {
+        const accessToken = await getToken();
+        ensureScope(`https://${
+        import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID
+      }.api.crm4.dynamics.com/.default`);
+        if (!accessToken) {
+            throw new Error('Access token is missing');
+        }
+
+        const apiUrl = `https://${
+        import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID
+      }.api.crm4.dynamics.com/api/data/v9.1/msdyn_projecttasks`;
+
+        const response = await fetch(apiUrl, {
+            method  : 'GET',
+            headers : {
+                'Authorization'    : `Bearer ${accessToken}`,
+                'OData-MaxVersion' : '4.0',
+                'OData-Version'    : '4.0',
+                'Accept'           : 'application/json',
+                'Content-Type'     : 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        const modData = data.value.filter((item) => item._msdyn_project_value === import.meta.env.VITE_MSDYN_PROJECT_ID);
+        return modData;
+    }
+    catch (error) {
+        console.error('Error fetching project tasks:', error);
+        throw error;
+    }
+}
+
+export async function getProjectTaskDependencies() {
+    try {
+        const accessToken = await getToken();
+        ensureScope(`https://${
+      import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID
+    }.api.crm4.dynamics.com/.default`);
+        if (!accessToken) {
+            throw new Error('Access token is missing');
+        }
+
+        const apiUrl = `https://${
+      import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID
+    }.api.crm4.dynamics.com/api/data/v9.1/msdyn_projecttaskdependencies`;
+
+        const response = await fetch(apiUrl, {
+            method  : 'GET',
+            headers : {
+                'Authorization'    : `Bearer ${accessToken}`,
+                'OData-MaxVersion' : '4.0',
+                'OData-Version'    : '4.0',
+                'Accept'           : 'application/json',
+                'Content-Type'     : 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        const modData = data.value.filter((item) => item._msdyn_project_value === import.meta.env.VITE_MSDYN_PROJECT_ID);
+        return modData;
+    }
+    catch (error) {
+        console.error('Error fetching project task dependencies:', error);
+        throw error;
+    }
+}
+
+export async function createProjectTask(projectId, projectBucketId, operationSetId, record) {
+    const accessToken = await getToken();
+    ensureScope(`https://${import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID}.api.crm4.dynamics.com/.default`);
+    if (!accessToken) {
+        throw new Error('Access token is missing');
+    }
+
+    const apiUrl = `https://${import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID}.api.crm4.dynamics.com/api/data/v9.1/msdyn_PssCreateV1`;
+    const payload = {
+        'Entity' : {
+            'msdyn_subject'                  : record.name,
+            'msdyn_start'                    : `${record.startDate.toISOString()}`,
+            'msdyn_finish'                   : `${record.endDate.toISOString()}`,
+            '@odata.type'                    : 'Microsoft.Dynamics.CRM.msdyn_projecttask',
+            'msdyn_project@odata.bind'       : `msdyn_projects(${projectId})`,
+            'msdyn_projectbucket@odata.bind' : `msdyn_projectbuckets(${projectBucketId})`,
+            '_msdyn_parenttask_value'        : record.parentId,
+            'msdyn_progress'                 : record.percentDone / 100
+            // 'msdyn_displaysequence'          : record.parentIndex + 1
+        },
+        'OperationSetId' : operationSetId
+    };
+
+    const response = await fetch(apiUrl, {
+        method  : 'POST',
+        headers : {
+            'Authorization'    : `Bearer ${accessToken}`,
+            'OData-MaxVersion' : '4.0',
+            'OData-Version'    : '4.0',
+            'Accept'           : 'application/json',
+            'Content-Type'     : 'application/json'
+        },
+        body : JSON.stringify(payload)
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+export async function createOperationSet(projectId, description) {
+    const accessToken = await getToken();
+    ensureScope(`https://${import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID}.api.crm4.dynamics.com/.default`);
+    if (!accessToken) {
+        throw new Error('Access token is missing');
+    }
+
+    const apiUrl = `https://${import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID}.api.crm4.dynamics.com/api/data/v9.1/msdyn_CreateOperationSetV1`;
+
+    const payload = {
+        'ProjectId'   : projectId,
+        'Description' : description
+    };
+
+    const response = await fetch(apiUrl, {
+        method  : 'POST',
+        headers : {
+            'Authorization'    : `Bearer ${accessToken}`,
+            'OData-MaxVersion' : '4.0',
+            'OData-Version'    : '4.0',
+            'Accept'           : 'application/json',
+            'Content-Type'     : 'application/json'
+        },
+        body : JSON.stringify(payload)
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.OperationSetId;
+}
+
+export async function executeOperationSet(operationSetId) {
+    const accessToken = await getToken();
+    ensureScope(`https://${import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID}.api.crm4.dynamics.com/.default`);
+    if (!accessToken) {
+        throw new Error('Access token is missing');
+    }
+
+    const apiUrl = `https://${import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID}.api.crm4.dynamics.com/api/data/v9.1/msdyn_ExecuteOperationSetV1`;
+
+    const payload = {
+        'OperationSetId' : operationSetId
+    };
+
+    const response = await fetch(apiUrl, {
+        method  : 'POST',
+        headers : {
+            'Authorization'    : `Bearer ${accessToken}`,
+            'OData-MaxVersion' : '4.0',
+            'OData-Version'    : '4.0',
+            'Accept'           : 'application/json',
+            'Content-Type'     : 'application/json'
+        },
+        body : JSON.stringify(payload)
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+export async function abandonOperationSet(operationSetId) {
+    const accessToken = await getToken();
+    ensureScope(`https://${import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID}.api.crm4.dynamics.com/.default`);
+    if (!accessToken) {
+        throw new Error('Access token is missing');
+    }
+
+    const apiUrl = `https://${import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID}.api.crm4.dynamics.com/api/data/v9.1/msdyn_AbandonOperationSetV1`;
+
+    const payload = {
+        'OperationSetId' : operationSetId
+    };
+
+    const response = await fetch(apiUrl, {
+        method  : 'POST',
+        headers : {
+            'Authorization'    : `Bearer ${accessToken}`,
+            'OData-MaxVersion' : '4.0',
+            'OData-Version'    : '4.0',
+            'Accept'           : 'application/json',
+            'Content-Type'     : 'application/json'
+        },
+        body : JSON.stringify(payload)
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+export async function updateProjectTask(projectId, projectBucketId, operationSetId, record) {
+    const accessToken = await getToken();
+    ensureScope(`https://${import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID}.api.crm4.dynamics.com/.default`);
+    if (!accessToken) {
+        throw new Error('Access token is missing');
+    }
+
+    const apiUrl = `https://${import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID}.api.crm4.dynamics.com/api/data/v9.1/msdyn_PssUpdateV1`;
+
+    const payloadObj = {
+        Entity : {
+            msdyn_projecttaskid              : record.id,
+            '@odata.type'                    : 'Microsoft.Dynamics.CRM.msdyn_projecttask',
+            'msdyn_project@odata.bind'       : `msdyn_projects(${projectId})`,
+            'msdyn_projectbucket@odata.bind' : `msdyn_projectbuckets(${projectBucketId})`
+        },
+        OperationSetId : operationSetId
+    };
+
+    if (record.name) {
+        payloadObj.Entity.msdyn_subject = record.name;
+    }
+    if (record.startDate) {
+        payloadObj.Entity.msdyn_start = `${record.startDate.toISOString()}`;
+    }
+    if (record.endDate) {
+        payloadObj.Entity.msdyn_finish = `${record.endDate.toISOString()}`;
+    }
+    if (record.parentId) {
+        payloadObj.Entity._msdyn_parenttask_value = record.parentId;
+    }
+    // 	The Progress, EffortCompleted, and EffortRemaining fields can be edited in Project for the Web, but they can't be edited in Project Operations: https://learn.microsoft.com/en-us/dynamics365/project-operations/project-management/schedule-api-preview
+    if (record.parentIndex) {
+        payloadObj.Entity.msdyn_displaysequence = record.parentIndex + 1;
+    }
+
+    const response = await fetch(apiUrl, {
+        method  : 'POST',
+        headers : {
+            'Authorization'    : `Bearer ${accessToken}`,
+            'OData-MaxVersion' : '4.0',
+            'OData-Version'    : '4.0',
+            'Accept'           : 'application/json',
+            'Content-Type'     : 'application/json'
+        },
+        body : JSON.stringify(payloadObj)
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+export async function deleteProjectTask(operationSetId, recordId) {
+    const accessToken = await getToken();
+    ensureScope(`https://${import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID}.api.crm4.dynamics.com/.default`);
+    if (!accessToken) {
+        throw new Error('Access token is missing');
+    }
+
+    const apiUrl = `https://${import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID}.api.crm4.dynamics.com/api/data/v9.1/msdyn_PssDeleteV1`;
+
+    const payload = {
+        'EntityLogicalName' : 'msdyn_projecttask',
+        'RecordId'          : recordId,
+        'OperationSetId'    : operationSetId
+    };
+
+    const response = await fetch(apiUrl, {
+        method  : 'POST',
+        headers : {
+            'Authorization'    : `Bearer ${accessToken}`,
+            'OData-MaxVersion' : '4.0',
+            'OData-Version'    : '4.0',
+            'Accept'           : 'application/json',
+            'Content-Type'     : 'application/json'
+        },
+        body : JSON.stringify(payload)
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+export async function createProjectTaskDependency(projectId, operationSetId, record) {
+    const accessToken = await getToken();
+    ensureScope(`https://${import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID}.api.crm4.dynamics.com/.default`);
+    if (!accessToken) {
+        throw new Error('Access token is missing');
+    }
+
+    const apiUrl = `https://${import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID}.api.crm4.dynamics.com/api/data/v9.1/msdyn_PssCreateV1`;
+
+    const payload = {
+        'Entity' : {
+            'msdyn_PredecessorTask@odata.bind'    : `msdyn_projecttasks(${record.fromEvent.id ? record.fromEvent.id : record.fromEvent})`,
+            'msdyn_SuccessorTask@odata.bind'      : `msdyn_projecttasks(${record.toEvent.id ? record.toEvent.id : record.toEvent})`,
+            'msdyn_projecttaskdependencylinktype' : 1,
+            '@odata.type'                         : 'Microsoft.Dynamics.CRM.msdyn_projecttaskdependency',
+            'msdyn_Project@odata.bind'            : `msdyn_projects(${projectId})`
+        },
+        'OperationSetId' : operationSetId
+    };
+
+    const response = await fetch(apiUrl, {
+        method  : 'POST',
+        headers : {
+            'Authorization'    : `Bearer ${accessToken}`,
+            'OData-MaxVersion' : '4.0',
+            'OData-Version'    : '4.0',
+            'Accept'           : 'application/json',
+            'Content-Type'     : 'application/json'
+        },
+        body : JSON.stringify(payload)
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+export async function deleteProjectTaskDependency(recordId, operationSetId) {
+    const accessToken = await getToken();
+    ensureScope(`https://${import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID}.api.crm4.dynamics.com/.default`);
+    if (!accessToken) {
+        throw new Error('Access token is missing');
+    }
+
+    const apiUrl = `https://${import.meta.env.VITE_MICROSOFT_DYNAMICS_ORG_ID}.api.crm4.dynamics.com/api/data/v9.1/msdyn_PssDeleteV1`;
+
+    const payload = {
+        'EntityLogicalName' : 'msdyn_projecttaskdependency',
+        'RecordId'          : recordId,
+        'OperationSetId'    : operationSetId
+    };
+
+    const response = await fetch(apiUrl, {
+        method  : 'POST',
+        headers : {
+            'Authorization'    : `Bearer ${accessToken}`,
+            'OData-MaxVersion' : '4.0',
+            'OData-Version'    : '4.0',
+            'Accept'           : 'application/json',
+            'Content-Type'     : 'application/json'
+        },
+        body : JSON.stringify(payload)
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+}
--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 import { Gantt } from '@bryntum/gantt';
 import '@bryntum/gantt/gantt.stockholm.css';
+import { signIn } from './auth.js';
+import { abandonOperationSet, createOperationSet, createProjectTask, createProjectTaskDependency, deleteProjectTask, deleteProjectTaskDependency, executeOperationSet, getProjectTaskDependencies, getProjectTasks, updateProjectTask } from './crudFunctions.js';
+import CustomTaskModel from './lib/CustomTaskModel.js';
+
+const signInLink = document.getElementById('signin');
 
 const gantt = new Gantt({
     appendTo   : 'gantt',
@@ -7,13 +12,221 @@ const gantt = new Gantt({
     timeZone   : 'UTC',
     date       : new Date(2024, 10, 1),
     project    : {
-        tasksData : [
-
-            { id : 1, name : 'Create docs', startDate : '2024-10-07', endDate : '2024-10-18' },
-            { id : 2, name : 'Write guides', startDate : '2024-10-21', endDate : '2024-10-31' }
-        ],
-        dependenciesData : [
-            { fromTask : 1, toTask : 2 }
-        ]
+        taskStore : {
+            transformFlatData : true,
+            modelClass        : CustomTaskModel
+        },
+        writeAllFields : true
+    },
+    listeners : {
+        dataChange : function(event) {
+            updateMicrosoftProject(event);
+        }
     }
 });
+
+async function displayUI() {
+    await signIn();
+
+    // Hide sign in link and initial UI
+    signInLink.style = 'display: none';
+    const content = document.getElementById('content');
+    content.style = 'display: block';
+
+    try {
+        const projectTasksPromise = getProjectTasks();
+        const getProjectDependenciesPromise = getProjectTaskDependencies();
+        const [projectTasks, projectDependencies] = await Promise.all([
+            projectTasksPromise,
+            getProjectDependenciesPromise
+        ]);
+
+        const ganttTasks = [];
+        projectTasks.forEach((event) => {
+            const startDateUTC = new Date(event.msdyn_start);
+            // Convert to local timezone
+            const startDateLocal = new Date(
+                startDateUTC.getTime() - startDateUTC.getTimezoneOffset() * 60000
+            );
+            const finishDateUTC = new Date(event.msdyn_finish);
+            // Convert to local timezone
+            const finishDateLocal = new Date(
+                finishDateUTC.getTime() - finishDateUTC.getTimezoneOffset() * 60000
+            );
+            ganttTasks.push({
+                id                : event.msdyn_projecttaskid,
+                parentId          : event._msdyn_parenttask_value,
+                name              : event.msdyn_subject,
+                startDate         : startDateLocal,
+                endDate           : finishDateLocal,
+                percentDone       : event.msdyn_progress * 100,
+                parentIndex       : parseInt(event.msdyn_displaysequence) + 1,
+                manuallyScheduled : true,
+                outlineLevel      : event.msdyn_outlinelevel
+            });
+        });
+        ganttTasks.sort((a, b) => a.parentIndex - b.parentIndex);
+        gantt.project.tasks = ganttTasks;
+
+        const ganttDependencies = [];
+        projectDependencies.forEach((dep) => {
+            ganttDependencies.push({
+                id   : dep.msdyn_projecttaskdependencyid,
+                from : dep._msdyn_predecessortask_value,
+                to   : dep._msdyn_successortask_value
+            });
+        });
+        gantt.project.dependencies = ganttDependencies;
+    }
+    catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+signInLink.addEventListener('click', displayUI);
+
+async function updateMicrosoftProject({ action, record, store, records }) {
+    const storeId = store.id;
+    if (storeId === 'tasks') {
+        if (action === 'update') {
+            if (`${record.id}`.startsWith('_generated')) {
+                if (!record.name) return;
+                let operationSetId = '';
+                const projectId = import.meta.env.VITE_MSDYN_PROJECT_ID;
+                const projectBucketId = import.meta.env.VITE_MSDYN_PROJECTBUCKET_VALUE;
+                const description = 'Create operation set for new project task';
+                try {
+                    operationSetId = await createOperationSet(projectId, description);
+                    const createProjectTaskResponse = await createProjectTask(projectId, projectBucketId, operationSetId, record);
+                    await executeOperationSet(operationSetId);
+                    // update id
+                    gantt.project.taskStore.applyChangeset({
+                        updated : [
+                            // Will set proper id for added task
+                            {
+                                $PhantomId : record.id,
+                                id         : createProjectTaskResponse['<OperationSetResponses>k__BackingField'][3].Value
+                            }
+                        ]
+                    });
+                    return;
+                }
+                catch (error) {
+                    await abandonOperationSet(operationSetId);
+                    console.error('Error:', error);
+                }
+            }
+            else {
+                let operationSetId = '';
+                const projectId = import.meta.env.VITE_MSDYN_PROJECT_ID;
+                const projectBucketId = import.meta.env.VITE_MSDYN_PROJECTBUCKET_VALUE;
+                const description = 'Create operation set for updating a project task';
+
+                try {
+                    operationSetId = await createOperationSet(projectId, description);
+                    await updateProjectTask(projectId, projectBucketId, operationSetId, record);
+                    await executeOperationSet(operationSetId);
+                    return;
+                }
+                catch (error) {
+                    await abandonOperationSet(operationSetId);
+                    console.error('Error:', error);
+                }
+            }
+        }
+        if (action === 'remove') {
+            const recordsData = records.map((record) => record.data);
+            recordsData.forEach(async(record) => {
+                if (record.id.startsWith('_generated')) return;
+                let operationSetId = '';
+                const projectId = import.meta.env.VITE_MSDYN_PROJECT_ID;
+                const description = 'Create operation set for deleting project task';
+                try {
+                    operationSetId = await createOperationSet(projectId, description);
+                    await deleteProjectTask(operationSetId, record.id);
+                    await executeOperationSet(operationSetId);
+                    return;
+                }
+                catch (error) {
+                    await abandonOperationSet(operationSetId);
+                    console.error('Error:', error);
+                }
+            });
+        }
+    }
+    if (storeId === 'dependencies') {
+        const recordsData = records.map((record) => record.data);
+        recordsData.forEach(async(record) => {
+            if (action === 'update') {
+                if (`${record.id}`.startsWith('_generated')) {
+                    // create new dependency
+                    let operationSetId = '';
+                    const projectId = import.meta.env.VITE_MSDYN_PROJECT_ID;
+                    const description = 'Create operation set for new project task dependency';
+                    try {
+                        operationSetId = await createOperationSet(projectId, description);
+                        const createProjectTaskDependencyResponse = await createProjectTaskDependency(projectId, operationSetId, record);
+                        await executeOperationSet(operationSetId);
+                        // update id
+                        gantt.project.dependencyStore.applyChangeset({
+                            updated : [
+                                // Will set proper id for added task
+                                {
+                                    $PhantomId : record.id,
+                                    id         : JSON.parse(createProjectTaskDependencyResponse.OperationSetResponse)['<OperationSetResponses>k__BackingField'][3].Value
+                                }
+                            ]
+                        });
+                        return;
+                    }
+                    catch (error) {
+                        await abandonOperationSet(operationSetId);
+                        console.error('Error:', error);
+                    }
+                }
+                else {
+                    // 1. delete old dependency
+                    let operationSetId = '';
+                    let description = '';
+                    const projectId = import.meta.env.VITE_MSDYN_PROJECT_ID;
+                    description = 'Operation set for updating a project task dependency: delete old and create new';
+                    try {
+                        operationSetId = await createOperationSet(projectId, description);
+                        await deleteProjectTaskDependency(record.id, operationSetId);
+                        const createProjectTaskDependencyResponse = await createProjectTaskDependency(projectId, operationSetId, record);
+                        await executeOperationSet(operationSetId);
+                        // update id
+                        gantt.project.dependencyStore.applyChangeset({
+                            updated : [
+                                // Will set proper id for added task
+                                {
+                                    $PhantomId : record.id,
+                                    id         : JSON.parse(createProjectTaskDependencyResponse.OperationSetResponse)['<OperationSetResponses>k__BackingField'][3].Value
+                                }
+                            ]
+                        });
+                    }
+                    catch (error) {
+                        await abandonOperationSet(operationSetId);
+                        await abandonOperationSet(operationSetId);
+                        console.error('Error:', error);
+                    }
+                }
+            }
+            if (action === 'remove') {
+                let operationSetId = '';
+                const projectId = import.meta.env.VITE_MSDYN_PROJECT_ID;
+                const description = 'Create operation set for deleting project task dependency';
+                try {
+                    operationSetId = await createOperationSet(projectId, description);
+                    await deleteProjectTaskDependency(record.id, operationSetId);
+                    await executeOperationSet(operationSetId);
+                }
+                catch (error) {
+                    await abandonOperationSet(operationSetId);
+                    console.error('Error:', error);
+                }
+            }
+        });
+    }
+}
